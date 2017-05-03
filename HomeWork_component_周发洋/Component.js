0f15c979d04367ab6f002500df0ea2b2622dfb6f
zhoufayang104 <350542619@qq.com>
2017-05-03 10:41:14 +0800

    g

--- HomeWork_component_周发洋/Component.js
+++ HomeWork_component_周发洋/Component.js
@@ -2,19 +2,19 @@ Vue.component('vue-table',{
     template:"<table :class='className'><thead><tr>\
         <th style='width:20px'></th>\
         <th v-for='cell in cellconfig'>{{ cell.title}}</th>\
-        <th>操作</th>\
+        <th style='width:60px'>操作</th>\
         </tr></thead>\
         <tbody><tr v-for='(user,index) in users' :class='{trSelected:user.completed}'>\
         <td style='text-align:center;width=20px'>\
-        <input type='checkbox' :disabled='user.editable' :key='index' v-model='user.completed'/></td>\
-        <td v-for='cell in cellconfig'>\
+        <input type='checkbox' :disabled='user.editable' :key='index' v-model='user.completed' @click='doCheckBoxClick(index)'/></td>\
+        <td v-for='cell in cellconfig'  @click='doTdClick(index)'>\
             <input v-if='user.editable && cell.type==text ' type='text' v-model='user[cell.cellName]'/>\
             <select v-else-if='user.editable && cell.type==select ' v-model='user[cell.cellName]'>\
                 <option v-for='option in cell.options' :value='option'>{{option}}</option>\
             </select>\
             <label v-else>{{user[cell.cellName]}}</label>\
         </td>\
-        <td><button @click='doEdit(index)'>{{user.editable | editShow}}</button></td>\
+        <td style='text-align:center;width:60px'><button @click='doEdit(index)'>{{user.editable | editShow}}</button></td>\
         </tr></tbody></table>",
     props: {
         users:Array,
@@ -29,7 +29,15 @@ Vue.component('vue-table',{
         doEdit:function(index){
             this.users[index].editable=!this.users[index].editable;
             this.users[index].completed=false;
-            this.$emit('do-edit');
+        },
+        doTdClick:function(index){
+            if(!this.users[index].editable){
+                this.users[index].completed=!this.users[index].completed;
+            }
+            this.$emit('get-selectedcount',index);
+        },
+        doCheckBoxClick:function(index){
+            this.$emit('get-selectedcount',index);
         }
     },
     filters:{
@@ -49,7 +57,22 @@ new Vue({
             {cellName:"age",title:"年龄",type:"text"},
             {cellName:"sex",title:"性别",type:"select",options:['男','女']}
         ],
-        total:0
+        selectedTotal:0
+    },
+    computed:{
+        total:function(){
+            return this.users.length;
+        },
+        maleTotal:function(){
+            return _.filter(this.users,function(user){
+                return user.sex=='男'
+            }).length;
+        },
+        femaleTotal:function(){
+            return _.filter(this.users,function(user){
+                return user.sex=='女'
+            }).length;
+        }
     },
     methods: {
         AddUser:function(){
@@ -58,11 +81,7 @@ new Vue({
             this.age=18;
             this.sex='男'
         },
-        refresh:function(){
-            this.total+=1;
-        },
         DelUser:function(){
-            
             var arr=[];
             for(var i=0;i<this.users.length;i++){
                 if(this.users[i].completed==false){
@@ -70,6 +89,13 @@ new Vue({
                 }
             }
             this.users=arr;
+        },
+        getSelectedCount:function(i){
+            if(this.users[i].completed){
+                this.selectedTotal+=1;
+            }else{
+                this.selectedTotal-=1;
+            }
         },
         deepCopy:function(o){
             if (o instanceof Array) {
